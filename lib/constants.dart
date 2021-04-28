acd5ed34a8d77540697459ab1fa6e12948fdf2f0
manoj <manojsaharia45@gmail.com>
2021-04-28 15:50:16 +0530

    second commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 
 const String daily_green_leaf = "Daily Green Leaf";
 const String daily_field_report = "Daily Field Report";
-const String daily_raw_material = "Daily Raw Material";
+const String daily_raw_material = "Daily Raw Materials";
 const String daily_production = "Daily Production";
